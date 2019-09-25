9cfcc6193a6861f29e41b5e39451188db66091cc
prasadrayudu <43725655+prasadrayudu@users.noreply.github.com>
2019-09-25 20:31:45 +0530

    forgetting git commands  New python lession pushed to master

---
+++ python/string.py
@@ -0,0 +1,44 @@
+var = "something"
+print(var)
+var1 = "Guru99!"
+var2 = "Software Testing"
+print("var1[0]:",var1[0])
+print("var2[0:8]:",var2[0:8])
+print (var1*2)
+print("u" in var1)
+print("1" not in var1)
+
+x = "Hello World!"
+print(x[:6])
+print(x[0:6] + "Guru99")
+
+oldstring = 'I like Guru99'
+newstring = oldstring.replace('like', 'love')
+print(newstring)
+print(newstring.lower())
+print(newstring.upper())
+print(newstring.capitalize())
+
+print(":".join("Python"))
+x = 'prasadrayudu'
+print(''.join(reversed(x)))
+word="guru99 career guru99"
+print(word.split(' '))
+word="guru99 career guru99"
+print(word.split('r'))
+
+x = "Guru99"
+x.replace("Guru99","Python")
+print(x)
+y = '1'
+y.replace('1','2')
+print(y)
+
+
+
+
+
+
+
+
+
---
+++ python/variable.py
@@ -0,0 +1,29 @@
+f = 0
+print(f)
+f = 'prasad'
+print(f)
+f = 'nice boy'
+print(f)
+
+a = 'Guru'
+b = 99
+print(a)
+print(b)
+print(a+str(b))
+
+
+c = 101
+print(c)
+
+def somefunction():
+	c = ' I am learning Python'
+	print(c)
+somefunction()
+
+def somefunction2():
+	c = ' I still dont know what will happen'
+	print(c)
+somefunction2()
+print(c)
+del c
+print(c)
